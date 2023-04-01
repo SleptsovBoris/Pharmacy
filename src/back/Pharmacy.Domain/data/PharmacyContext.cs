9cd452dee353f66f864e8a0e1c59f1914185d43d
SleptsovBoris <71390697+SleptsovBoris@users.noreply.github.com>
2023-04-01 13:09:49 +0300

    Поправили отображение списка заказов, и добавили ссылки на картинки в списке товаров

--- src/back/Pharmacy.Domain/data/PharmacyContext.cs
+++ src/back/Pharmacy.Domain/data/PharmacyContext.cs
@@ -14,12 +14,12 @@ public class PharmacyContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Drug>().HasData(
-            new Drug(1, "", "Мирамистин", "Описание", 200),
-            new Drug(2, "", "Витамин С", "Описание", 180),
-            new Drug(3, "", "Стрепсилс", "Описание", 350),
-            new Drug(4, "", "Но-шпа", "Описание", 500),
-            new Drug(5, "", "Парацетамол", "Описание", 300),
-            new Drug(6, "", "Називин", "Описание", 200)
+            new Drug(1, "http://127.0.0.1:9000/product-images/miramistin.jpeg", "Мирамистин", "Описание", 200),
+            new Drug(2, "http://127.0.0.1:9000/product-images/vitamin-c.jpg", "Витамин С", "Описание", 180),
+            new Drug(3, "http://127.0.0.1:9000/product-images/strepsils.jpg", "Стрепсилс", "Описание", 350),
+            new Drug(4, "http://127.0.0.1:9000/product-images/no-spa.png", "Но-шпа", "Описание", 500),
+            new Drug(5, "http://127.0.0.1:9000/product-images/paracetamol.jpeg", "Парацетамол", "Описание", 300),
+            new Drug(6, "http://127.0.0.1:9000/product-images/nasivin.jpg", "Називин", "Описание", 200)
         );
     }
 }
