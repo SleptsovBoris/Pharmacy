81ac7ba1b346a7757f923cba05685bf9ca52e79a
SleptsovBoris <71390697+SleptsovBoris@users.noreply.github.com>
2023-04-02 13:45:34 +0300

    Изменили управление состоянием списка заказов с useState на Redux

--- src/front/src/components/Cart/components/CartItemsTab/CartItemsTab.tsx
+++ src/front/src/components/Cart/components/CartItemsTab/CartItemsTab.tsx
@@ -1,5 +1,4 @@
 import { Button, Divider } from 'antd';
-import { IOrder } from '../../Cart';
 import CartList from '../CartList';
 import './CartItemsTab.scss';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,12 +8,9 @@ import {
   removeProduct,
   setProductCount,
 } from 'redux/ducks/cart_list';
+import { IOrder, addOrder } from 'redux/ducks/orders_list';
 
-interface IProps {
-  handleAddOrder: (order: IOrder) => void;
-}
-
-const CartItemsTab: React.FC<IProps> = (props: IProps) => {
+const CartItemsTab: React.FC = () => {
   const cartState = useSelector((state: RootState) => state.cartList);
   const dispatch = useDispatch();
   const totalPrice = cartState.items.reduce(
@@ -28,9 +24,9 @@ const CartItemsTab: React.FC<IProps> = (props: IProps) => {
     const order: IOrder = {
       products: cartState.items,
       totalPrice: totalPrice,
-      date: new Date().toDateString(),
+      date: new Date(),
     };
-    props.handleAddOrder(order);
+    dispatch(addOrder(order));
 
     dispatch(clearCart());
   };
--- src/front/src/components/Cart/components/OrdersTab/OrdersTab.tsx
+++ src/front/src/components/Cart/components/OrdersTab/OrdersTab.tsx
@@ -1,24 +1,28 @@
 import { Collapse } from 'antd';
-import { IOrder } from '../../Cart';
 import CartList from '../CartList';
 import './OrdersTab.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from 'redux/rootReducer';
 
 const { Panel } = Collapse;
 
-interface IProps {
-  orders: IOrder[];
-  handleRemoveItemFromOrder: (order: IOrder) => void;
-}
-
-const OrdersTab: React.FC<IProps> = (props: IProps) => {
+const OrdersTab: React.FC = () => {
+  const ordersState = useSelector((state: RootState) => state.ordersList);
   return (
     <>
-      {props.orders.map(order => (
+      {ordersState.items.map(order => (
+        //eslint-disable-next-line
         <div key={order.id!} className="order-collapse-wrapper">
-          <Collapse key={order.id!}>
+          <Collapse>
             <Panel
+              //eslint-disable-next-line
               key={order.id!}
-              header={`${order.date} - ${order.totalPrice} ₽`}
+              header={`${new Date(order.date).toLocaleDateString(
+                'ru-Ru'
+              )} ${new Date(order.date).toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit',
+              })} - ${order.totalPrice} ₽`}
             >
               <CartList cartItems={order.products} />
             </Panel>
